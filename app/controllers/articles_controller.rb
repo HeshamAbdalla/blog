3968469666df8b5adc40897eadfce31f4e4c048c
HeshamAbdalla <shamoh702@gmail.com>
2015-10-18 23:43:47 -0700

    Create article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,4 +3,23 @@ class ArticlesController < ApplicationController
   def index
 
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      flash[:success] = "Article has been created"
+      redirect_to articles_path  
+    end
+  end
 end
+
+
+  private
+
+  def article_params
+    params.require(:article).permit(:body, :title)
+  end
