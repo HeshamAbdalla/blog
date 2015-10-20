9dd2872ed10ff6895b33627584b432b8461eb2f5
HeshamAbdalla <shamoh702@gmail.com>
2015-10-20 02:33:48 -0700

    Style Signup page

--- spec/features/signing_users_spec.rb
+++ spec/features/signing_users_spec.rb
@@ -6,8 +6,8 @@ RSpec.feature "Users signup" do
 
     click_link "Sign up"
     fill_in "Email", with: "user@example.com"
-    fill_in "Password", with: "Password"
-    fill_in "Password confirmation", with: "Password"
+    fill_in "Password", with: "55555555"
+    fill_in "Password confirmation", with: "55555555"
     click_button "Sign up"
 
     expect(page).to have_content("You have signed up successfully.")
